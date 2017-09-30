830f4df26ad34da3000ff35e78bd8a6f527607de
Eduardo Monges <emonges24@gmail.com>
2017-09-29 22:47:32 -0400

    Creacion del componente home, plantillas y estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { app_routing } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
